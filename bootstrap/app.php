6525e3997481b1009ce8be6d91f849b3d871f138
mara-vicentina <maravicentina73@outlook.com>
2025-03-13 19:09:01 -0300

    fix: ajusta configurações do cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'auth' => Authenticate::class,
         ]);
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
